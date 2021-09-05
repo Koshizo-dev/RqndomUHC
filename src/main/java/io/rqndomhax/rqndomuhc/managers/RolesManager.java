b0a976ae017a18bb65cac7b2acd411ae69dde4d0
RqndomHax <paul.codepro@gmail.com>
2021-09-05 16:03:16 +0200

    Update RolesManager.java

--- src/main/java/io/rqndomhax/rqndomuhc/managers/RolesManager.java
+++ src/main/java/io/rqndomhax/rqndomuhc/managers/RolesManager.java
@@ -5,11 +5,13 @@ import io.rqndomhax.uhcapi.role.RRole;
 import io.rqndomhax.uhcapi.role.RRoleManager;
 import io.rqndomhax.uhcapi.utils.RValue;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RolesManager extends RValue implements RRoleManager {
+
+    final RValue activeRoles = new RValue();
 
     @Override
     public void createRole(String roleKey, Class<? extends RRole> role) {
@@ -28,17 +30,61 @@ public class RolesManager extends RValue implements RRoleManager {
 
     @Override
     public HashMap<String, RRole> getRoles() {
-        return null;
+        return (HashMap<String, RRole>) castObjects(RRole.class);
     }
 
     @Override
-    public Set<RGamePlayer> getGamePlayer(RRole role) {
-        return null;
+    public void enableRole(RRole role) {
+        activeRoles.addObject(getKey(role), role);
+    }
+
+    @Override
+    public void enableRole(String roleKey) {
+        activeRoles.addObject(roleKey, getObject(roleKey));
+    }
+
+    @Override
+    public void disableRole(RRole role) {
+        activeRoles.removeObject(role);
+    }
+
+    @Override
+    public void disableRole(String roleKey) {
+        activeRoles.removeKey(roleKey);
+    }
+
+    @Override
+    public HashMap<String, RRole> getActiveRoles() {
+        return (HashMap<String, RRole>) activeRoles.castObjects(RRole.class);
+    }
+
+    @Override
+    public Set<RGamePlayer> getGamePlayers(RRole role, Set<RGamePlayer> gamePlayers) {
+        return gamePlayers.stream().filter(gamePlayer -> gamePlayer.getPlayerInfos().getObjects().values().stream().anyMatch(info -> role.getClass().isInstance(info))).collect(Collectors.toSet());
     }
 
     @Override
     public RRole getRole(RGamePlayer gamePlayer) {
-        return null;
+        return gamePlayer.getPlayerInfos().getObjects().values().stream().filter(info -> info instanceof RRole).map(info -> (RRole) info).findFirst().orElse(null);
     }
 
+    @Override
+    public void dispatchRoles(Set<RGamePlayer> gamePlayers) {
+        List<RGamePlayer> remainingPlayers = new ArrayList<>(gamePlayers);
+
+        for (RRole role : getActiveRoles().values()) {
+            if (remainingPlayers.size() == 0)
+                return;
+            RGamePlayer gamePlayer = remainingPlayers.get(0);
+            try {
+                RRole newRole = (RRole) role.getClass().getDeclaredConstructors()[0].newInstance(gamePlayer);
+                newRole.onRoleGiven();
+                gamePlayer.getPlayerInfos().addObject("role", newRole);
+                remainingPlayers.remove(gamePlayer);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
 }

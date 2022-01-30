0404309abb8071d8e7a435422bd48e48ea74204c
RqndomHax <paul.codepro@gmail.com>
2022-01-30 16:15:31 +0100

    :bug: fixed enchant command executor

--- src/main/java/io/rqndomhax/rqndomuhc/managers/GameManager.java
+++ src/main/java/io/rqndomhax/rqndomuhc/managers/GameManager.java
@@ -5,6 +5,7 @@
 
 package io.rqndomhax.rqndomuhc.managers;
 
+import io.rqndomhax.rqndomuhc.commands.CEnchant;
 import io.rqndomhax.rqndomuhc.commands.CForce;
 import io.rqndomhax.rqndomuhc.commands.CHost;
 import io.rqndomhax.rqndomuhc.commands.CHostSaveInventory;
@@ -81,6 +82,7 @@ public class GameManager implements UHCAPI {
         Objects.requireNonNull(plugin.getCommand("save")).setExecutor(new CHostSaveInventory(this));
         Objects.requireNonNull(plugin.getCommand("host")).setExecutor(new CHost(this));
         Objects.requireNonNull(plugin.getCommand("force")).setExecutor(new CForce(this));
+        Objects.requireNonNull(plugin.getCommand("enchant")).setExecutor(new CEnchant(this));
         Bukkit.getLogger().log(Level.INFO, "[RqndomUHC] Registered commands.");
 
         this.inventories = new DynamicInventoryManager(this);
